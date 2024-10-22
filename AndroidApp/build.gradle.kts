2fcd48d4e0f2c4ec231da078909b31da39290513
xaeliudzyh <gtsagolov8@gmail.com>
2024-10-22 22:16:51 +0300

    fixed build.gradle.kts problem

--- AndroidApp/build.gradle.kts
+++ AndroidApp/build.gradle.kts
@@ -1,6 +1,22 @@
-plugins {
-    // ...
+// build.gradle.kts (корневой проект)
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Используйте актуальную версию Android Gradle Plugin
+        classpath("com.android.tools.build:gradle:8.0.2")
+        // Используйте совместимую версию Kotlin Gradle Plugin
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
+        // Обновите версию плагина Google Services до 4.3.15
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}
 
-    // Add the dependency for the Google services Gradle plugin
-    id("com.google.gms.google-services") version "4.4.2" apply false
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }

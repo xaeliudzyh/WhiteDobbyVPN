7312b22df1a7d0e55678130c7b88e9ff57cea411
xaeliudzyh <gtsagolov8@gmail.com>
2024-12-01 20:00:13 +0300

    added some more dependencies and created class for workmanager tests

---
+++ AndroidClient/WhiteDobbyVPN/app/src/main/java/com/example/whitedobby/WorkManagerTest.kt
@@ -0,0 +1,11 @@
+package com.example.whitedobby
+
+import android.content.Context
+import androidx.work.Data
+import androidx.work.testing.TestWorkerBuilder
+import androidx.test.core.app.ApplicationProvider
+import junit.framework.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.*
+class WorkManagerTest {
+}
